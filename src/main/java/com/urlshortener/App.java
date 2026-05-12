21625c81060ff4529f623f8927ee1c8654ad7adf
shalini <hey@gamil.com>
2026-05-12 22:58:34 +0530

    first commit

---
+++ src/main/java/com/urlshortener/App.java
@@ -0,0 +1,13 @@
+package com.urlshortener;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
